8f0f3f364d8cde1c8672d4e5356a43a2691307e8
Bernd Hirschmann <bhi@guidnew.com>
2020-06-24 15:33:22 +0200

    include options pattern in existing AutoMapper extension library

---
+++ AutoMapperOptions/AutoMapperOptions.Module3/Module3.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+
+// code from AutoMapper.Extensions.Microsoft.DependencyInjection TestApp
+namespace AutoMapperOptions.Module3
+{
+    public class ScanSource
+    {
+    }
+
+    public class ScanDest
+    {
+    }
+
+    public class ScanSource2
+    {
+    }
+
+    public class ScanDest2
+    {
+        public int ResolvedValue { get; set; }
+    }
+
+    public class ScanProfile1 : Profile
+    {
+        public ScanProfile1()
+        {
+            CreateMap<ScanSource, ScanDest>();
+        }
+    }
+
+    public class ScanProfile2 : Profile
+    {
+        public ScanProfile2()
+        {
+            CreateMap<ScanSource2, ScanDest2>()
+                .ForMember(d => d.ResolvedValue, opt => opt.MapFrom<ScanDependencyResolver>());
+        }
+    }
+
+    public class ScanDependencyResolver : IValueResolver<object, object, int>
+    {
+        private readonly IScanSomeService _service;
+
+        public ScanDependencyResolver(IScanSomeService service)
+        {
+            _service = service;
+        }
+
+        public int Resolve(object source, object destination, int destMember, ResolutionContext context)
+        {
+            return _service.Modify(destMember);
+        }
+    }
+
+    public interface IScanSomeService
+    {
+        int Modify(int value);
+    }
+
+    public class ScanFooService : IScanSomeService
+    {
+        private readonly int _value;
+
+        public ScanFooService(int value)
+        {
+            _value = value;
+        }
+
+        public int Modify(int value) => value + _value;
+    }
+}
--- AutoMapperOptions/AutoMapperOptions/Program.cs
+++ AutoMapperOptions/AutoMapperOptions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using AutoMapperOptions.Module1;
 using AutoMapperOptions.Module2;
+using AutoMapperOptions.Module3;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -13,6 +14,8 @@ namespace AutoMapperOptions
             IServiceCollection services = new ServiceCollection();
             services.AddModule1();
             services.AddModule2();
+            services.AddAutoMapper(typeof(ScanSource));
+            services.AddTransient<IScanSomeService>(sp => new ScanFooService(6));
 
             var provider = services.BuildServiceProvider();
 
@@ -22,7 +25,8 @@ namespace AutoMapperOptions
             Console.WriteLine($"Service1 result: {model1.Prop1}, {model1.Prop2}");
 
             // code from AutoMapper.Extensions.Microsoft.DependencyInjection TestApp
-            // services are defined in Module2
+            // services are defined in Module2 and Module3
+            // servcies of Module2 are registered manually and configured via options; services of Module3 via assembly scanning
             using (var scope = provider.CreateScope())
             {
                 var mapper = scope.ServiceProvider.GetService<IMapper>();
@@ -39,6 +43,9 @@ namespace AutoMapperOptions
 
                 var dest = mapper.Map<Dest2>(new Source2());
                 Console.WriteLine(dest.ResolvedValue);
+
+                var scanDest = mapper.Map<ScanDest2>(new ScanSource2());
+                Console.WriteLine(scanDest.ResolvedValue);
             }
 
             Console.ReadKey(true);
